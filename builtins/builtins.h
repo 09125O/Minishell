acaee73798d741cb62ac62c3cfef3f863d35f3db
root <root@DESKTOP-1M4M1LM.localdomain>
2025-02-05 16:13:18 +0100

    Ajout de cd

--- builtins/builtins.h
+++ builtins/builtins.h
@@ -6,12 +6,17 @@
 /*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 15:53:47 by root              #+#    #+#             */
-/*   Updated: 2025/02/05 15:54:04 by root             ###   ########.fr       */
+/*   Updated: 2025/02/05 16:10:39 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BUILTINS_H
 # define BUILTINS_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <errno.h>
 
 
 #endif
