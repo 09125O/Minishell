357f51f7ec9fe9098430ebae9df557a093f21e57
root <root@DESKTOP-1M4M1LM.localdomain>
2025-02-09 17:12:29 +0100

    ajout de echo

--- builtins/builtins.h
+++ builtins/builtins.h
@@ -6,7 +6,7 @@
 /*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 15:53:47 by root              #+#    #+#             */
-/*   Updated: 2025/02/09 17:04:00 by root             ###   ########.fr       */
+/*   Updated: 2025/02/09 17:12:17 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,7 @@ int		ft_cd(char **args, char **env);
 int		ft_env(char **env);
 int		ft_unset(char **args, char **env);
 int		ft_export(char **args, char **env);
+int	ft_echo(char **args);
 void	ft_exit(int status);
 
 #endif
